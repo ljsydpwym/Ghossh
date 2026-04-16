63dea8d477e366ceec304e2497a0c89259e581be
ljsydpwym <ljsudpwym@gmail.com>
2026-04-16 09:33:57 +0300

    chore: bump android version to 0.1.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.jossephus.chuchu"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
